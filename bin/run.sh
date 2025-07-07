f555b5afac6527dc7e37d85b54e1f58f2c9b83a1
Adarsh Senghani <adarshco077@gmail.com>
2025-07-07 18:07:27 +0530

    fix: updated run script to properly delete existing container

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 docker build -t paisa-web-ui .
-docker remove paisa-web-ui -f
+docker rm -f paisa-web-ui
 docker run --name paisa-web-ui --add-host=host.docker.internal:host-gateway -p 8003:8003 -d paisa-web-ui:latest
